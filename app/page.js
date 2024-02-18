d2fdc3c08ce43f64057460444bcb053d0394f8d8
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-18 17:30:46 -0600

    add color

--- app/page.js
+++ app/page.js
@@ -9,33 +9,34 @@ export default function Home() {
   return (
     <>
       <Box>
-        {/* <div>
-        <FormGroup>
+        {/* <FormGroup>
           <FormControlLabel
             control={<Switch defaultChecked />}
             label='Charges'
           />
           <FormControlLabel label='Checkouts' />
-        </FormGroup>
-      </div> */}
+        </FormGroup> */}
         <div>
           <TextField
             required
             id='outlined-required'
             label='Required'
             defaultValue='Hello World'
+            color='success'
           />
           <TextField
             disabled
             id='outlined-disabled'
             label='Disabled'
             defaultValue='Hello World'
+            color='success'
           />
           <TextField
             id='outlined-password-input'
             label='Password'
             type='password'
             autoComplete='current-password'
+            color='success'
           />
         </div>
         <Button variant='contained'>Hello world</Button>
