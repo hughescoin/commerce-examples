68fdb92cfe065bdcde45980ab95d27a728d6bd3f
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-18 17:13:30 -0600

    add fields and buttons

--- app/page.js
+++ app/page.js
@@ -1,9 +1,52 @@
-import Button from '@mui/material/Button';
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  TextField,
+} from '@mui/material/Button';
 
 export default function Home() {
   return (
-    <>
+    <Box
+      component='form'
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete='off'
+    >
+      <div>
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch defaultChecked />}
+            label='Charges'
+          />
+          <FormControlLabel label='Checkouts' />
+        </FormGroup>
+      </div>
+      <div>
+        <TextField
+          required
+          id='outlined-required'
+          label='Required'
+          defaultValue='Hello World'
+        />
+        <TextField
+          disabled
+          id='outlined-disabled'
+          label='Disabled'
+          defaultValue='Hello World'
+        />
+        <TextField
+          id='outlined-password-input'
+          label='Password'
+          type='password'
+          autoComplete='current-password'
+        />
+      </div>
       <Button variant='contained'>Hello world</Button>;
-    </>
+    </Box>
   );
 }
