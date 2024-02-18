f2a3dcd9c7b5efc1eb0fa1f14ad7e877fb875b59
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-18 17:27:35 -0600

    remove formgroup

--- app/page.js
+++ app/page.js
@@ -1,23 +1,15 @@
-import {
-  Box,
-  Button,
-  FormControlLabel,
-  FormGroup,
-  Switch,
-  TextField,
-} from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function Home() {
   return (
-    <Box
-      component='form'
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete='off'
-    >
-      <div>
+    <>
+      <Box>
+        {/* <div>
         <FormGroup>
           <FormControlLabel
             control={<Switch defaultChecked />}
@@ -25,28 +17,29 @@ export default function Home() {
           />
           <FormControlLabel label='Checkouts' />
         </FormGroup>
-      </div>
-      <div>
-        <TextField
-          required
-          id='outlined-required'
-          label='Required'
-          defaultValue='Hello World'
-        />
-        <TextField
-          disabled
-          id='outlined-disabled'
-          label='Disabled'
-          defaultValue='Hello World'
-        />
-        <TextField
-          id='outlined-password-input'
-          label='Password'
-          type='password'
-          autoComplete='current-password'
-        />
-      </div>
-      <Button variant='contained'>Hello world</Button>;
-    </Box>
+      </div> */}
+        <div>
+          <TextField
+            required
+            id='outlined-required'
+            label='Required'
+            defaultValue='Hello World'
+          />
+          <TextField
+            disabled
+            id='outlined-disabled'
+            label='Disabled'
+            defaultValue='Hello World'
+          />
+          <TextField
+            id='outlined-password-input'
+            label='Password'
+            type='password'
+            autoComplete='current-password'
+          />
+        </div>
+        <Button variant='contained'>Hello world</Button>
+      </Box>
+    </>
   );
 }
