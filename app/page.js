91baaecf239a2b681d00ed70f0dacf3b7e4e6cd8
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-18 17:43:02 -0600

    remove unused packages

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Switch from '@mui/material/Switch';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function Home() {
   return (
