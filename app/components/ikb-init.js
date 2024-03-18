359c37a884d1d57361c65578f7145a352ecc0bc8
Patrick Hughes <patrick.hughes@coinbase.com>
2024-03-18 11:22:32 -0500

    fix server side render error

--- app/components/ikb-init.js
+++ app/components/ikb-init.js
@@ -2,17 +2,17 @@ import { initOnRamp } from '@coinbase/cbpay-js';
 import Image from 'next/image';
 import compactBuyButton from '../../public/compactBuy.png';
 import { initOnRampConfig } from '../../constants';
-import { useMemo, useRef, useState } from 'react';
+import { useMemo, useRef, useState, useEffect } from 'react';
 import { destWallets } from '../../constants';
 
 export default function IKB() {
   const [isReady, setIsReady] = useState(false);
   const amount = 10;
   const userId = 796;
-  const onrampInstance = useRef(); //reference a value not needed for rendering //access DOM nodes inside components
+  const onrampInstance = useRef();
 
-  const widgetOptions = useMemo(() => {
-    return {
+  const widgetOptions = useMemo(
+    () => ({
       appId: process.env.NEXT_PUBLIC_APP_ID,
       target: '#cbpay-container',
       widgetParameters: {
@@ -28,38 +28,26 @@ export default function IKB() {
         partnerUserId: userId,
         defaultExperience: 'buy',
       },
-      onSuccess: () => {
-        console.log('success');
-      },
-      onExit: (event) => {
-        console.log('exit', event);
-      },
+      onSuccess: () => console.log('success'),
+      onExit: (event) => console.log('exit', event),
       onEvent: async (event) => {
         console.log('COINBASE EVENT => ', event);
-        if (event.eventName === 'open') {
-          // await buyCrypto("started")
-        }
-        if (event.eventName === 'success') {
-          // await buyCrypto("success")
-        }
         console.log('onEvent ', event);
-        // event stream
       },
       experienceLoggedIn: 'embedded',
       experienceLoggedOut: 'embedded',
       closeOnExit: true,
       closeOnSuccess: true,
-    };
-  }, [destWallets, amount]);
+    }),
+    [amount]
+  );
 
-  // init
-  initOnRamp(widgetOptions, (error, instance) => {
-    console.log('error ', error);
-    if (instance) {
-      onrampInstance.current = instance;
-      setIsReady(true);
-
-      onrampInstance.current.open();
-    }
-  });
+  useEffect(() => {
+    initOnRamp(widgetOptions, (error, instance) => {
+      if (instance) {
+        onrampInstance.current = instance;
+        setIsReady(true);
+      }
+    });
+  }, [widgetOptions]);
 }
